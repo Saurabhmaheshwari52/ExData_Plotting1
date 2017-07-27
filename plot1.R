c28ec4a70556da645eca3c5b23b735a964a09ac3
Saurabhmaheshwari52 <saurabhmaheshwari52@gmail.com>
2017-07-27 20:01:31 +0530

    Plots and Figures
    Find the plots and figures for assignment 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## Creating the required data
+lines = readLines("household_power_consumption.txt")
+subs = grep("^[12]/2/2007", substr(lines, 1,10))
+sublines = lines[subs]
+data = read.table(text = sublines, sep = ";",
+                  col.names = c("Date","Time", 
+                                "Global_active_power", "Global_reactive_power",
+                                "Voltage", "Global_intensity", 
+                                "Sub_metering_1", "Sub_metering_2", 
+                                "Sub_metering_3"))
+## creating the graph
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
